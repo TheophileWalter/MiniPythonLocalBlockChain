01f3104a3971ea8fe1be88b00284e69cb1872cf1
AlexisMarembert <alexismarembert@gmail.com>
2018-06-20 16:23:39 +0200

    update Accounts.py

--- Accounts.py
+++ Accounts.py
@@ -41,14 +41,16 @@ class Accounts:
     # Params : self, accountId
     def _check_if_account_already_exists(self, accountId):
         fileName = self.nodePath+"/"+accountId
-        return os.path.isfile(file_name)
+        return os.path.isfile(fileName)
 
     # Method : create account
     # Params : self, amount
     def create_account(self, amount):
         hashCreated = Accounts._get_new_hash(self)
+        while Accounts._check_if_account_already_exists(self, hashCreated):
+            Accounts.create_account(self, amount)
+        
         fileName = str(self.nodePath+"/"+hashCreated)
-        print(fileName)
         accountFileToModify = open(fileName, 'w')
         accountFileToModify.write(str(amount))
         accountFileToModify.close()
@@ -58,14 +60,12 @@ class Accounts:
 # TESTS
 if __name__ == '__main__':
     newAccount1 = Accounts('node_1')
+    # test create
     account11 = newAccount1.create_account(500)
-
-    """print(open('./nodes/node_1/').read())
-    print(account11)
+    print(account11+" : "+open('./nodes/node_1/accounts/'+account11).read()+" [=? 500]")
+    # test modify
     newAccount1.modify_account(account11, 400)
-    
-
-    newAccount2 = Accounts('node_2')
-    print(newAccount2._get_accounts_list())
-    newAccount2._chec k_if_account_already_exists"""
-    
+    print(account11+" : "+open('./nodes/node_1/accounts/'+account11).read()+" [=? 400]")
+    # test _check_if_account_already_exists
+    print("File already exists : "+str(newAccount1._check_if_account_already_exists(account11))+" [=? True]")
+    print("File already exists : "+str(newAccount1._check_if_account_already_exists("20374ca3ee149e19d5e0a024f7062836"))+" [=? False]")

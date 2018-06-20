6433110cc7f1438d480cba46e6a66d5e058a58d6
Alice <alice.lejeune@u-psud.fr>
2018-06-20 15:44:58 +0200

    modif create_t and transacParent

--- create_template.sh
+++ create_template.sh
@@ -48,14 +48,14 @@ _create_content () {
     transaction)
         echo "from ${j}.${randomStringOld}"         >> ${filePath}
         echo "to ${j}.${randomStringNew}"           >> ${filePath}
-        echo "amount $(echo $RANDOM)"         >> ${filePath}
-        echo "fees $(echo $RANDOM)"           >> ${filePath}
+        echo "amount $(echo $RANDOM) units"         >> ${filePath}
+        echo "fees $(echo $RANDOM) units"           >> ${filePath}
         ;;
     pendingTransaction)
         echo "from ${j}.${randomStringNew}"         >> ${filePath}
         echo "to ${j}.${randomStringOld}"           >> ${filePath}
-        echo "amount $(echo $RANDOM)"         >> ${filePath}
-        echo "fees $(echo $RANDOM)"           >> ${filePath}
+        echo "amount $(echo $RANDOM) units"         >> ${filePath}
+        echo "fees $(echo $RANDOM) units"           >> ${filePath}
         ;;
     *)
         echo ""                                     >> ${filePath}

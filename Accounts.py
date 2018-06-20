4655580a290741f055642a4e57c677ec7b6a82ca
AlexisMarembert <alexismarembert@gmail.com>
2018-06-20 11:38:24 +0200

    update Accounts.py

--- Accounts.py
+++ Accounts.py
@@ -1,6 +1,9 @@
 # coding=utf-8
 #!/bin/python3
 
+import os
+import re
+
 class Accounts(object):
 
     # Method : init
@@ -8,6 +11,7 @@ class Accounts(object):
     def __init__(self, uniqueID, amount):
         self.uniqueID = uniqueID
         self.amount = amount
+        self.accountsFilesList = []
 
 
     # Method : append a new account in account.txt
@@ -15,6 +19,16 @@ class Accounts(object):
     def _create_account(self):
         accountFile = open('accounts.txt', 'w+')
         accountFile.write(str(self.uniqueID)+":"+str(self.amount))
+    
+    # Method : get list of account nodes
+    # Params : self
+    def _get_accounts_list(self):
+        for root, dirs, files in os.walk("nodes/"):  
+            for fileName in files:
+                if re.match(r'^.*accounts.txt$', fileName):
+                    self.accountsFilesList.append()
+
+        for p in self.accountsFilesList: print print(p)
 
 
 newAccount1 = Accounts('1.e970dc85732370a2', '500')
